6851c0b1c4c043d5992b42350e40d49d22e7294c
Joceeeee <100770558+Joceeeee@users.noreply.github.com>
2023-04-15 23:36:01 +0800

    double checked working and adjust spacing

--- js/script.js
+++ js/script.js
@@ -1,51 +1,51 @@
 function addRSStoDOM(data) {
-    let itemsContainer = document.createElement('DIV');
-    for (let i = 0, t = data.items.length; i < t; ++i) {
-      let item = data.items[i];  
-      let itemContainer = document.createElement('DIV');
-      let itemLinkElement = document.createElement('A');
-      itemLinkElement.setAttribute('href', item.link);
-      itemLinkElement.innerText = item.title;    
-      let itemTitleElement = document.createElement('H2');
-      itemTitleElement.appendChild(itemLinkElement);
-      let itemDescriptionElement = document.createElement('P');
-      itemDescriptionElement.innerHTML = item.description;
-      itemContainer.appendChild(itemTitleElement);
-      itemContainer.appendChild(itemDescriptionElement);
-      itemsContainer.appendChild(itemContainer);
-    }
-    let titleElement = document.createElement('H1');
-    titleElement.innerText = data.feed.title;
-    content.appendChild(titleElement);
-    content.appendChild(itemsContainer);
+  let itemsContainer = document.createElement('DIV');
+  for (let i = 0, t = data.items.length; i < t; ++i) {
+    let item = data.items[i];  
+    let itemContainer = document.createElement('DIV');
+    let itemLinkElement = document.createElement('A');
+    itemLinkElement.setAttribute('href', item.link);
+    itemLinkElement.innerText = item.title;    
+    let itemTitleElement = document.createElement('H2');
+    itemTitleElement.appendChild(itemLinkElement);
+    let itemDescriptionElement = document.createElement('P');
+    itemDescriptionElement.innerHTML = item.description;
+    itemContainer.appendChild(itemTitleElement);
+    itemContainer.appendChild(itemDescriptionElement);
+    itemsContainer.appendChild(itemContainer);
   }
-  
-  var content = document.getElementsByTagName('main')[0]
-  
-  var xhr = new XMLHttpRequest();
-  
-  xhr.onload = function(){
-    /* added clear function to remove else message when success */
-    /* ref: https://stackoverflow.com/questions/22593759/how-do-i-clear-inner-html */
-    content.innerHTML = "";
-    if (xhr.status >=200 && xhr.status < 300) {
-      json = JSON.parse(xhr.responseText);
-      console.log(json);
-      addRSStoDOM(json);
-    } else{
-      console.log("The request failed");
-      content.innerHTML = "The request failed, please check your RSS URL.";
-    }
+  let titleElement = document.createElement('H1');
+  titleElement.innerText = data.feed.title;
+  content.appendChild(titleElement);
+  content.appendChild(itemsContainer);
+}
+
+var content = document.getElementsByTagName('main')[0]
+
+var xhr = new XMLHttpRequest();
+
+xhr.onload = function(){
+  /* added clear function to remove else message when success */
+  /* ref: https://stackoverflow.com/questions/22593759/how-do-i-clear-inner-html */
+  content.innerHTML = "";
+  if (xhr.status >=200 && xhr.status < 300) {
+    json = JSON.parse(xhr.responseText);
+    console.log(json);
+    addRSStoDOM(json);
+  } else{
+    console.log("The request failed");
+    content.innerHTML = "The request failed, please check your RSS URL.";
   }
-  
-  
-  let addFeedButton = document.getElementById("add-feed");
-  let newRSSInput = document.getElementById("rss-input");
-  
-  function onAddRSSClicked(event) {
-    let URL = newRSSInput.value;
-    newRSSInput.value = "";
-    xhr.open('GET', 'https://api.rss2json.com/v1/api.json?rss_url=' + URL);
-    xhr.send();
-  }
-  addFeedButton.addEventListener('click', onAddRSSClicked);
+}
+
+
+let addFeedButton = document.getElementById("add-feed");
+let newRSSInput = document.getElementById("rss-input");
+
+function onAddRSSClicked(event) {
+  let URL = newRSSInput.value;
+  newRSSInput.value = "";
+  xhr.open('GET', 'https://api.rss2json.com/v1/api.json?rss_url=' + URL);
+  xhr.send();
+}
+addFeedButton.addEventListener('click', onAddRSSClicked);
